92e5a72fdfd59d9d6e44eb037a9f58f2c069c070
korangg <mustafa5252005@gmail.com>
2025-08-12 10:32:17 +0800

    PUSH SEND FEEDBACK
    This code will link to database

---
+++ send_feedback.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+$conn = mysqli_connect("localhost", "root", "", "ecommerce_db");
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = mysqli_real_escape_string($conn, trim($_POST["name"]));
+    $email = mysqli_real_escape_string($conn, trim($_POST["email"]));
+    $message = mysqli_real_escape_string($conn, trim($_POST["message"]));
+
+    mysqli_query($conn, "INSERT INTO feedback (name, email, message, submitted_at) 
+                         VALUES ('$name', '$email', '$message', NOW())");
+
+    header("Location: contact_us.php?sent=1");
+    exit();
+}
+?>
