219a5a5765b20f51dd031530fdfe1e0491d0fda3
Peashy77 <zzudin05@gmail.com>
2025-09-25 03:50:24 +0800

    UPDATE STAFFDASHBOARD.PHP

--- temp_dashboard/pages/staffDashboard.php
+++ temp_dashboard/pages/staffDashboard.php
@@ -90,27 +90,7 @@ $orderList = mysqli_query($conn, "SELECT * FROM orders ORDER BY order_date DESC"
                 </g>
               </svg>
             </div>
-            <span class="nav-link-text ms-1">View Customer</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link  " href="../pages/viewFeedback - S.php">
-            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-              <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                <title>office</title>
-                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                  <g transform="translate(-1869.000000, -293.000000)" fill="#FFFFFF" fill-rule="nonzero">
-                    <g transform="translate(1716.000000, 291.000000)">
-                      <g id="office" transform="translate(153.000000, 2.000000)">
-                        <path class="color-background opacity-6" d="M12.25,17.5 L8.75,17.5 L8.75,1.75 C8.75,0.78225 9.53225,0 10.5,0 L31.5,0 C32.46775,0 33.25,0.78225 33.25,1.75 L33.25,12.25 L29.75,12.25 L29.75,3.5 L12.25,3.5 L12.25,17.5 Z"></path>
-                        <path class="color-background" d="M40.25,14 L24.5,14 C23.53225,14 22.75,14.78225 22.75,15.75 L22.75,38.5 L19.25,38.5 L19.25,22.75 C19.25,21.78225 18.46775,21 17.5,21 L1.75,21 C0.78225,21 0,21.78225 0,22.75 L0,40.25 C0,41.21775 0.78225,42 1.75,42 L40.25,42 C41.21775,42 42,41.21775 42,40.25 L42,15.75 C42,14.78225 41.21775,14 40.25,14 Z M12.25,36.75 L7,36.75 L7,33.25 L12.25,33.25 L12.25,36.75 Z M12.25,29.75 L7,29.75 L7,26.25 L12.25,26.25 L12.25,29.75 Z M35,36.75 L29.75,36.75 L29.75,33.25 L35,33.25 L35,36.75 Z M35,29.75 L29.75,29.75 L29.75,26.25 L35,26.25 L35,29.75 Z M35,22.75 L29.75,22.75 L29.75,19.25 L35,19.25 L35,22.75 Z"></path>
-                      </g>
-                    </g>
-                  </g>
-                </g>
-              </svg>
-            </div>
-            <span class="nav-link-text ms-1">View Feedback</span>
+            <span class="nav-link-text ms-1">Manage Customer</span>
           </a>
         </li>
         <li class="nav-item">
@@ -155,11 +135,31 @@ $orderList = mysqli_query($conn, "SELECT * FROM orders ORDER BY order_date DESC"
             <span class="nav-link-text ms-1">Manage Order</span>
           </a>
         </li>
+		<li class="nav-item">
+          <a class="nav-link  " href="../pages/viewFeedback - S.php">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <title>office</title>
+                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                  <g transform="translate(-1869.000000, -293.000000)" fill="#FFFFFF" fill-rule="nonzero">
+                    <g transform="translate(1716.000000, 291.000000)">
+                      <g id="office" transform="translate(153.000000, 2.000000)">
+                        <path class="color-background opacity-6" d="M12.25,17.5 L8.75,17.5 L8.75,1.75 C8.75,0.78225 9.53225,0 10.5,0 L31.5,0 C32.46775,0 33.25,0.78225 33.25,1.75 L33.25,12.25 L29.75,12.25 L29.75,3.5 L12.25,3.5 L12.25,17.5 Z"></path>
+                        <path class="color-background" d="M40.25,14 L24.5,14 C23.53225,14 22.75,14.78225 22.75,15.75 L22.75,38.5 L19.25,38.5 L19.25,22.75 C19.25,21.78225 18.46775,21 17.5,21 L1.75,21 C0.78225,21 0,21.78225 0,22.75 L0,40.25 C0,41.21775 0.78225,42 1.75,42 L40.25,42 C41.21775,42 42,41.21775 42,40.25 L42,15.75 C42,14.78225 41.21775,14 40.25,14 Z M12.25,36.75 L7,36.75 L7,33.25 L12.25,33.25 L12.25,36.75 Z M12.25,29.75 L7,29.75 L7,26.25 L12.25,26.25 L12.25,29.75 Z M35,36.75 L29.75,36.75 L29.75,33.25 L35,33.25 L35,36.75 Z M35,29.75 L29.75,29.75 L29.75,26.25 L35,26.25 L35,29.75 Z M35,22.75 L29.75,22.75 L29.75,19.25 L35,19.25 L35,22.75 Z"></path>
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </svg>
+            </div>
+            <span class="nav-link-text ms-1">View Feedback</span>
+          </a>
+        </li>
         <li class="nav-item mt-3">
           <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account pages</h6>
         </li>
         <li class="nav-item">
-          <a class="nav-link  " href="../pages/profile.html">
+          <a class="nav-link  " href="../pages/profile.php">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
               <svg width="12px" height="12px" viewBox="0 0 46 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>customer-support</title>
@@ -180,7 +180,7 @@ $orderList = mysqli_query($conn, "SELECT * FROM orders ORDER BY order_date DESC"
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link  " href="../pages/sign-in.html">
+          <a class="nav-link  " href="../pages/../../logout.php">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
               <svg width="12px" height="12px" viewBox="0 0 40 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>document</title>
@@ -196,7 +196,7 @@ $orderList = mysqli_query($conn, "SELECT * FROM orders ORDER BY order_date DESC"
                 </g>
               </svg>
             </div>
-            <span class="nav-link-text ms-1">Sign In</span>
+            <span class="nav-link-text ms-1">Sign Out</span>
           </a>
         </li>
         <li class="nav-item">
@@ -231,22 +231,14 @@ $orderList = mysqli_query($conn, "SELECT * FROM orders ORDER BY order_date DESC"
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
             <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
-            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">View Customer</li>
+            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Manage Customer</li>
           </ol>
-          <h6 class="font-weight-bolder mb-0">View Customer</h6>
-		  <a class="btn btn-outline-primary btn-sm mb-0" href="profile.php">Profile</a>
+          <h6 class="font-weight-bolder mb-0">Manage Customer</h6>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
           </div>
           <ul class="navbar-nav  justify-content-end">
-            <li class="nav-item d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-body font-weight-bold px-0">
-                <i class="fa fa-user me-sm-1"></i>
-                <a href="../../logout.php" class="d-sm-inline d-none">Sign Out</a>
-              </a>
-            </li>
-
             <li class="nav-item px-3 d-flex align-items-center">
               <a href="javascript:;" class="nav-link text-body p-0">
                 <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>
@@ -303,12 +295,12 @@ $orderList = mysqli_query($conn, "SELECT * FROM orders ORDER BY order_date DESC"
     <!-- End Navbar -->
     <div class="container-fluid py-4">
 	
-<!-- View Customer -->
+<!-- Manage Customer -->
 <div class="row">
   <div class="col-12">
     <div class="card mb-4">
       <div class="card-header pb-0">
-        <h6>View Customer</h6>
+        <h6>Manage Customer</h6>
       </div>
       <div class="card-body px-0 pt-0 pb-2">
         <div class="table-responsive p-0">
@@ -332,7 +324,7 @@ $orderList = mysqli_query($conn, "SELECT * FROM orders ORDER BY order_date DESC"
                 <td><p class="text-xs text-secondary mb-0"><?= htmlspecialchars($u["phone"]) ?></p></td>
                 <td><p class="text-xs mb-0"><?= $u["verified"] ? "✅" : "❌" ?></p></td>
                 <td class="text-center">
-                  <a href="adminDashboard.php?user_id=<?= $u['id'] ?>" class="text-info font-weight-bold text-xs">Edit</a> | 
+                  <a href="staffDashboard.php?user_id=<?= $u['id'] ?>" class="text-info font-weight-bold text-xs">Edit</a> | 
                   <a href="?delete_user=<?= $u["id"] ?>" 
                      class="text-danger font-weight-bold text-xs" 
                      onclick="return confirm('Delete this user?')">Delete</a>
